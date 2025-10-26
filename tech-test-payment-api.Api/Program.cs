137a3190f3babec575bcf801c1b375f762f2399d
thalesallan.dev <trabalhecomthales@gmail.com>
2025-10-26 16:08:22 -0300

    fix(program): remove unused HSTS comment and app.UseStaticFiles() due to missing wwwroot folder

--- tech-test-payment-api.Api/Program.cs
+++ tech-test-payment-api.Api/Program.cs
@@ -34,16 +34,13 @@ app.UseSwaggerUI(c =>
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Tech test payment API v1");
 });
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-// Removed app.UseStaticFiles() because the project does not include a wwwroot folder
 
 app.UseRouting();
 
