c87ac40d8093fb8f2459a38706f38b60e426fab6
thalesallan.dev <trabalhecomthales@gmail.com>
2025-10-26 12:08:58 -0300

    Upgrade project to .NET 8.0 and update package references

--- tech-test-payment-api.Api/Program.cs
+++ tech-test-payment-api.Api/Program.cs
@@ -1,4 +1,3 @@
-using Microsoft.OpenApi.Models;
 using System.Text.Json.Serialization;
 using tech_test_payment.Api.ConfigurationInjection;
 
@@ -7,23 +6,23 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-// Dependecy Injection
+// Dependency Injection
 var services = builder.Services;
 UseCaseConfig.ConfigureServices(services);
 RepositoryConfig.ConfigureServices(services);
 ValidatorConfig.ConfigureServices(services);
 AutoMapperConfig.ConfigureServices(services);
 
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Tech test payment api", Version = "v1" });
-});
 
 // Configure SerializerOptions
 builder.Services.AddControllers()
-    .AddJsonOptions(options => { 
-        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()); 
-    });
+   .AddJsonOptions(options =>
+   {
+       options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+   });
+
+// Register Swagger generator
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -35,7 +34,6 @@ app.UseSwaggerUI(c =>
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Tech test payment API v1");
 });
 
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -43,7 +41,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -53,7 +50,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+   name: "default",
+   pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
